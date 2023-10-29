d861db9f880fd668fe397a6c1206d58c1c63d56d
smsxgli <smsxgli@gmail.com>
2023-10-29 17:08:24 +0800

    Refactor build scripts.

--- cmake/cortex-m3.cmake
+++ cmake/cortex-m3.cmake
@@ -1,1 +1,16 @@
-set(ARCH_FLAGS -mcpu=cortex-m3 -mthumb)
+add_library(arch_intf INTERFACE)
+
+target_compile_options(
+    arch_intf
+    INTERFACE
+        -mcpu=cortex-m3
+        -mthumb
+)
+
+
+target_link_options(
+    arch_intf
+    INTERFACE
+        -mcpu=cortex-m3
+        -mthumb
+)
--- cmake/stm32f103zet6.cmake
+++ cmake/stm32f103zet6.cmake
@@ -1,2 +1,21 @@
-set(BOARD_MACRO STM32F10X_HD)
-set(STARTUP_SOURCE startup_stm32f10x_hd.c)
+add_library(bsp_intf INTERFACE)
+
+target_include_directories(
+    bsp_intf
+    INTERFACE
+        ${CMAKE_SOURCE_DIR}/firmware/cmsis/include
+)
+
+target_compile_definitions(
+    bsp_intf
+    INTERFACE
+        STM32F10X_HD
+)
+# we shall not compile this source file here
+add_library(bsp_src INTERFACE)
+
+target_sources(
+    bsp_src
+    INTERFACE
+        ${CMAKE_SOURCE_DIR}/firmware/cmsis/src/startup_stm32f10x_hd.c
+)
